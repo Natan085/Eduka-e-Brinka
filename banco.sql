c140a8705824d5b7486c6af5d6b84c847a0fedb5
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-08-14 19:52:29 -0300

    Update banco.sql
    Signed-off-by: Natan085 <119545067+Natan085@users.noreply.github.com>

--- banco.sql
+++ banco.sql
@@ -135,6 +135,7 @@ CREATE TABLE `aluno` (
   `nome_responsavel` VARCHAR(100) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
   `rg` VARCHAR(20) NOT NULL,
+  `cpf` VARCHAR (11) NOT NULL,
   `sexo` CHAR(1) NOT NULL,
   `data_nascimento` DATE NOT NULL,
   `telefone` VARCHAR(20) NOT NULL,
