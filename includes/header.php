f00827791f7b9d28fc864910fef549f887453326
Jfilho04 <129802247+Jfilho04@users.noreply.github.com>
2023-08-14 21:00:38 -0300

    Add files via upload
    Signed-off-by: Jfilho04 <129802247+Jfilho04@users.noreply.github.com>

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/header.css">
+  <title>Educa & Brinca</title>
+</head>
+
+<body>
+
+  <header>
+
+    <div class="dropdown">
+        <button class="dropbtn">MENU </button>
+      <div class="dropdown-content">
+        <a href="#">Financeiro</a>
+        <a href="#">Jogos</a>
+        <a href="#">Contato</a>
+        <a href="#">.</a>
+        <a href="#">.</a>
+        <a href="#">.</a>
+        <a href="#">.</a>
+      </div>
+    </div>
+
+
+    <div class="logo">
+      <a href="../pages/inicio.php">
+        <img src="../img/" alt="">
+      </a>
+    </div>
+    <div class="header-content">
+      <a href="../pages/inicio.php" class="projeto">Educa & Brinka
+        </a>
+    </div>
+
+  </header>
+</body>
+
+</html>
